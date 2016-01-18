2afbdb9f9cb9ebd19dc88f53d9844457b31adb09
Jeffrey Gisin <jgisin@gmail.com>
2016-01-18 14:49:25 -0700

    company names

--- web_scraper.rb
+++ web_scraper.rb
@@ -13,7 +13,7 @@ class WebScraper
     #binding.pry
     @search_form = @home_page.form_with(:action => "/jobs")
     @search_query = nil
-    @search_result
+    @search_result = nil
   end
 
 
@@ -24,15 +24,16 @@ class WebScraper
   end
 
   def search_results
-    @search_result = @search_query.search("//div[@class='serp-result-content']")
+    @search_result = @search_query.search("//div[@class='serp-result-content']")[0].search("//li[@class='employer']")
   end
 
   def get_company(result)
-    result.search("//li[@class='employer']")[0].children[4].children[0].text
+    result.children[4].children[0].text
   end
 
   def iterate
     @search_result.each do |result|
+      #binding.pry
       p get_company(result)
     end
   end
@@ -41,8 +42,8 @@ class WebScraper
 end
 
 web = WebScraper.new
-#binding.pry
 web.search("web", "aurora")
 #p web.search_results[0].search("//li[@class='employer']")[0].children[4].children[0].text
 web.search_results
+
 p web.iterate
